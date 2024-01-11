674ccdb49cedbc1c77ae96f6491ad6119fed45e2
mercedezvdw <mercedezvdwal@gmail.com>
2024-01-11 13:20:22 +0100

    Add classes

---
+++ classes/battery.py
@@ -0,0 +1,4 @@
+class Battery():
+    def __init__(self, positie, capaciteit):
+    self.positie = positie
+    self.capaciteit = capacaciteit
---
+++ classes/house.py
@@ -0,0 +1,5 @@
+class House():
+    def __init__(self, x, y, maxoutput):
+        self.x = x
+        self.y = y
+        self.maxoutput = maxoutput
