362254e731318e51585f6de50eda39bfdfb8204a
Rem3663 <rembrand@lepelaar.eu>
2024-01-23 12:30:29 +0100

    read/write data functions
    added read and write functions to be used

--- code/classes/readData.py
+++ code/classes/readData.py
@@ -5,3 +5,22 @@ class ReadData():
     def __init__(self, districtNumber, usedAlgorithm):
         self.districtNumber = districtNumber
         self.usedAlgorithm = usedAlgorithm
+    
+    def ReadExperimentData(self):
+        """
+        Reads all data from the corresponding file.
+        The format we use splits the data using ; instead of ',', due to the stored house, battery and cable data.
+        """
+        return None
+    
+    def ReadBestResult(self):
+        """
+        Returns the result with the lowest score and the corresponding map data.
+        """
+        return None
+
+    def ReadWorstResult(self):
+        """
+        Returns the result with the highest score and the corresponding map data.
+        """
+        return None
--- code/classes/writeData.py
+++ code/classes/writeData.py
@@ -5,3 +5,16 @@ class WriteData():
     def __init__(self, districtNumber, usedAlgorithm):
         self.districtNumber = districtNumber
         self.usedAlgorithm = usedAlgorithm
+
+    def WriteExperimentData(self):
+        """
+        Writes the result of a single experiment in a csv file.
+        The format we use splits the data using ; instead of ',', due to the stored house, battery and cable data.
+        """
+        return None
+    
+    def ClearExperimentData(self):
+        """
+        Clears all data from a file for a hard reset, when significant changes are made.
+        """
+        return None
