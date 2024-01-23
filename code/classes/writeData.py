8af40c6a0a0196c95a7200375924d16ec79b47fa
Rem3663 <rembrand@lepelaar.eu>
2024-01-23 15:19:31 +0100

    removed error in district 0 + data r/w changes

--- code/classes/writeData.py
+++ code/classes/writeData.py
@@ -6,18 +6,24 @@ class WriteData():
         self.districtNumber = districtNumber
         self.usedAlgorithm = usedAlgorithm
 
-    def WriteExperimentData(self):
+    def WriteExperimentData(self, total_costs, houses, batteries, cables):
         """
         Writes the result of a single experiment in a csv file.
         The format we use splits the data using ; instead of ',', due to the stored house, battery and cable data.
         format:
         total_costs; houses; batteries; cables
         """
+        ##text=List of strings to be written to file
+        with open(f'district-{self.districtNumber}_alg{self.usedAlgorithm}.csv','wb') as file:
+            file.write(f'{total_costs}; {houses}; {batteries}; {cables}')
 
-        return None
     
     def ClearExperimentData(self):
         """
         Clears all data from a file for a hard reset, when significant changes are made.
         """
-        return None
+        with open(f'district-{self.districtNumber}_alg{self.usedAlgorithm}.csv', 'r+') as file:
+            # read one line (header)
+            next(file)
+            # delete everything below that (all data)
+            file.truncate()
