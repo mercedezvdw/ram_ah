b3e15c02315e84a4eeb8df75c68161b00c31a046
Rem3663 <rembrand@lepelaar.eu>
2024-01-28 16:33:21 +0100

    data read/write reset
    it needs to be json, not csv, so total revamp is needed

--- code/classes/readData.py
+++ code/classes/readData.py
@@ -13,55 +13,8 @@ class ReadData():
     def ReadExperimentData(self):
         """
         Reads all data from the corresponding file.
-        The format we use splits the data using ; instead of ',', due to the stored house, battery and cable data.
-        format:
-        total_costs (int value); houses ([x,y,maxoutput]); batteries ([x,y,capacity]); cables ([x_beign,y_begin,x_end,y_end])
         """
-        # create lists to store data in and a row variable to use to read file
-        row = 0
-        file_total_costs = []
-        file_batteries = []
-        file_houses = []
-        file_cables = []
-
-        # read both files
-        input_file = open(f'data/results/district-{self.districtNumber}_alg{self.usedAlgorithmNumber}.csv', 'r')
-
-        # go through all rows and store data into lists
-        # BATTERIES
-        # read
-        for row_count in input_file:
-            total_costs = {}
-            batteries = {}
-            houses = {}
-            cables = {}
-
-            if row > 0:
-                row_count = row_count.strip()
-                if '"' in row_count:
-                    row_count = row_count.replace('"','')
-                data_split = row_count.split(';')
-                
-                # now that the data is split by ';', we can store all separate data formats
-                '''
-                # store data
-                total_costs[row-1] = int(data_split[0])
-                # houses
-                data_split[1].split(',')
-                houses[row-1] = House([int(data_split[0]), int(data_split[1])], float(data_split[2]))
-                # batteries
-                data_split[2].split(',')
-                batteries[row-1] = Battery([int(data_split[0]), int(data_split[1])], float(data_split[2]))
-                # cables
-                data_split[3].split(',')
-                cables[row-1] = CableSegment([int(data_split[0]), int(data_split[1])], float(data_split[2]))
-                #'''
-
-            row += 1
-
-        input_file.close()
-
-        return file_total_costs, file_batteries, file_houses, file_cables
+        return None
 
     
     def ReadBestResult(self):
--- code/classes/writeData.py
+++ code/classes/writeData.py
@@ -8,23 +8,13 @@ class WriteData():
 
     def WriteExperimentData(self, total_costs, houses, batteries, cables):
         """
-        Writes the result of a single experiment in a csv file.
-        The format we use splits the data using ; instead of ',', due to the stored house, battery and cable data.
-        format:
-        total_costs (int value); houses ([x,y,maxoutput]); batteries ([x,y,capacity]); cables ([x_beign,y_begin,x_end,y_end])
+        Writes the result of a single experiment in a .json file.
         """
-        ##text=List of strings to be written to file
-        with open(f'data/results/district-{self.districtNumber}_alg{self.usedAlgorithmNumber}.csv','wb') as file:
-            #write_houses = f'{houses}'
-            file.write(f'{total_costs}; {houses}; {batteries}; {cables}')
+        return None
 
     
     def ClearExperimentData(self):
         """
         Clears all data from a file for a hard reset, when significant changes are made.
         """
-        with open(f'district-{self.districtNumber}_alg{self.usedAlgorithmNumber}.csv', 'r+') as file:
-            # read one line (header)
-            next(file)
-            # delete everything below that (all data)
-            file.truncate()
+        return None
